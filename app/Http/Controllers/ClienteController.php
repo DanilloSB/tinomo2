08f7b6bbc5d72dbc86b4b14c94c5cde45a0361ff
Caique24-01 <caiqueleme100@gmail.com>
2021-12-08 12:30:46 -0300

    Correção ControllerCliente

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-use App\Models\Cliente;
+use App\Models\cliente;
 
 class ClienteController extends Controller
 {
