8fde2047d36af8d43872348f72a2510cd498440a
Caique24-01 <caiqueleme100@gmail.com>
2021-12-08 13:24:10 -0300

    Correção contact

---
+++ resources/views/site/contactnovo.blade.php
@@ -0,0 +1,63 @@
+@extends('site.master.layout')
+
+@section('content')
+
+    
+    <!-- ***** Contact Us Start ***** -->
+    <section class="section colored" id="contact-us">
+        <div class="container py-5">
+            <!-- ***** Section Title Start ***** -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="center-heading">
+                        <h2 class="section-title">Fale Conosco</h2>
+                    </div>
+                </div>
+                <div class="offset-lg-3 col-lg-6">
+                    <div class="center-text">
+                        <p>Digite sua dúvida que ela logo será respondida.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- ***** Section Title End ***** -->
+
+            <div class="row">
+
+                <!-- ***** Contact Form Start ***** -->
+                <div class="col-lg-8 col-md-6 col-sm-12">
+                    <div class="contact-form">
+                        <form id="contact" action="{{ route('registrar_contato') }}" method="post">
+                            @csrf 
+                          
+                            <div class="row">
+                            <div class="col-lg-6 col-md-12 col-sm-12">
+                              <fieldset>
+                                <input name="nome" type="text" class="form-control" placeholder="Nome Completo" required="required" data-error="O nome é obrigatório.">
+                              </fieldset>
+                            </div>
+                            <div class="col-lg-6 col-md-12 col-sm-12">
+                              <fieldset>
+                                <input name="email" type="email" class="form-control" placeholder="Endereço De E-Mail" required="required" data-error="É necessário um e-mail válido.">
+                              </fieldset>
+                            </div>
+                            <div class="col-lg-12">
+                              <fieldset>
+                                <textarea name="msg" rows="6" class="form-control" placeholder="Sua Mensagem" required="required" data-error="Por favor, deixe-nos uma mensagem."></textarea>
+                              </fieldset>
+                            </div>
+                            <div class="col-lg-12">
+                              <fieldset>
+                                <button type="submit" id="form-submit" class="main-button">Enviar</button>
+                              </fieldset>
+                            </div>
+                          </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- ***** Contact Form End ***** -->
+            </div>
+        </div>
+    </section>
+    <!-- ***** Contact Us End ***** --> 
+        
+@endsection
